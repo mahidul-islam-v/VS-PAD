2ec33dd17a42485c4f511435b0a0bb98bb7864cf
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-06-06 04:47:03 +0000

    Jun 6, 2026, 10:47 AM

--- my-mobile-app/MyApp/app/(tabs)/index.tsx
+++ my-mobile-app/MyApp/app/(tabs)/index.tsx
@@ -1,12 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 const app = () => {
     return (
-        <View>
-            <Text>Cofee Shop</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Cofee Shop</Text>
         </View>
     );
 };
 
 export default app;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+    },
+    text: {
+        color: "white",
+        fontSize: 42,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+});
