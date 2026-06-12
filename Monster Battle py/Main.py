81859e9bd8ccfd11fea7cdd7b597a39f71c37eda
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-06-12 16:37:47 +0000

    Jun 12, 2026, 10:37 PM

--- Monster Battle py/Main.py
+++ Monster Battle py/Main.py
@@ -4,17 +4,18 @@ from Veldora import Veldora
 def battle(e1, e2):
     e1.talk()
     e2.talk()
+    print('\n')
     round = 0
 
     while e1.hp>0 and e2.hp>0:
         round+=1
         print(f'------Round {round}------')
-        print(f'-------Fight------')
+        print(f'-------Fight-------')
         e1.ultimate()
         e2.ultimate()
         e1.atk()
         e2.atk()
-        e1.hp-= e2.ad
+        e1.hp-= e2.ad 
         e2.hp-= e1.ad
         print(f'{e1.get_name()}: {e1.hp}hp')
         print(f'{e2.get_name()}: {e2.hp}hp')
@@ -23,7 +24,7 @@ def battle(e1, e2):
     winner = e1.get_name() if e1.hp>0 else e2.get_name() if e2.hp>0 else "both"
     print(f'{winner} wins\n**{winner.lower()} smirks')
 
-p1 = Rimuru(50, 5)
-p2 = Veldora(100, 4)
+p1 = Rimuru(5, 50)
+p2 = Veldora(4, 100)
 
 battle(p1, p2)
