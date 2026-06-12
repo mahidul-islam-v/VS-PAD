d934853be68f75e4e8019e3515d4b6afe207f781
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-06-12 15:39:10 +0000

    Jun 12, 2026, 9:39 PM

--- Monster Battle py/Main.py
+++ Monster Battle py/Main.py
@@ -18,6 +18,10 @@ def battle(p1, p2):
         p2.hp-= p1.ad
         print(f'{p1.get_name()}: {p1.hp}hp')
         print(f'{p2.get_name()}: {p2.hp}hp')
+        print('\n')
+        
+    winner = p1.get_name() if p1.hp>0 else p2.get_name()
+    print(f'{winner} wins\n**{winner.lower()} smirks')
 
 p1 = Rimuru(50, 5)
 p2 = Veldora(100, 4)
