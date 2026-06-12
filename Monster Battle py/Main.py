bea6bee2cc70e085fd4a54581724662c59c94c43
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-06-12 16:43:06 +0000

    Jun 12, 2026, 10:43 PM

--- Monster Battle py/Main.py
+++ Monster Battle py/Main.py
@@ -16,13 +16,13 @@ def battle(e1, e2):
         e1.atk()
         e2.atk()
         e1.hp-= e2.ad if e2.ad<e1.hp else e1.hp
-        e2.hp-= e1.ad if e1.ad<e2.hp else e1.hp
+        e2.hp-= e1.ad if e1.ad<e2.hp else e2.hp
         print(f'{e1.get_name()}: {e1.hp}hp')
         print(f'{e2.get_name()}: {e2.hp}hp')
         print('\n')
         
     winner = e1.get_name() if e1.hp>0 else e2.get_name() if e2.hp>0 else "both"
-    print(f'{winner} wins\n**{winner.lower()} smirks')
+    print(f'### ⭐ {winner} wins ⭐ ###\n**{winner.lower()} smirks')
 
 p1 = Rimuru(5, 50)
 p2 = Veldora(4, 100)
--- Monster Battle py/Veldora.py
+++ Monster Battle py/Veldora.py
@@ -9,7 +9,7 @@ class Veldora(Enemy):
         print('Haha! Come brother, I shall entertain you.')
     
     def ultimate(self):
-        did_ult = random.random() < 0.05
+        did_ult = random.random() < 0.02
         if did_ult:
             self.ad+= 9999999
             print('Veldora goes Super Saiyan!!!!!')
