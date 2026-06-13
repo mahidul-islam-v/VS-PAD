562f8eb2e8c3c2e04815ab677754ec38d29a03b3
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-06-13 21:59:19 +0000

    Jun 14, 2026, 3:59 AM

--- Monster Battle py/Main.py
+++ Monster Battle py/Main.py
@@ -11,23 +11,23 @@ def battle(e1, e2):
     while e1.hp>0 and e2.hp>0:
         round+=1
         print(f'------Round {round}------')
-        time.sleep(1)
+        time.sleep(1*s)
         print(f'-------Fight!------')
-        time.sleep(0.5)
+        time.sleep(0.5*s)
         e1.ultimate()
         e2.ultimate()
         
-        time.sleep(0.5)
+        time.sleep(0.5*s)
         e1.atk()
-        time.sleep(0.5)
+        time.sleep(0.5*s)
         e2.atk()
-        time.sleep(0.5)
+        time.sleep(0.5*s)
         e1.hp = max(0, e1.hp - e2.ad)
         e2.hp = max(0, e2.hp - e1.ad)
         print(f'{e1.get_name()}: \033[32m{e1.hp}\033[0m hp')
-        time.sleep(0.5)
+        time.sleep(0.5*s)
         print(f'{e2.get_name()}: \033[32m{e2.hp}\033[0m hp')
-        time.sleep(2)
+        time.sleep(2*s)
 
         
     winner = e1.get_name() if e1.hp>0 else e2.get_name() if e2.hp>0 else "both"
@@ -41,6 +41,7 @@ p2h = 50
 
 arg = sys.argv
 al = len(arg)
+s = int(input("Enter game speed: \033[30m# Recommended speed is 10\033[0m"))
 
 if al != 1 and al != 5 :
     print("\033[1;34mInvalid number of arguments!\033[0m\nRead the documentation on github for further instructions.")
