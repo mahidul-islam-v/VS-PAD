2e1614d8595aa73ba99b1d776f53e86d8130d0d3
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-06-13 21:28:27 +0000

    Jun 14, 2026, 3:28 AM

--- Monster Battle py/Main.py
+++ Monster Battle py/Main.py
@@ -16,8 +16,8 @@ def battle(e1, e2):
         e2.ultimate()
         e1.atk()
         e2.atk()
-        e1.hp-= e2.ad if e2.ad<e1.hp else e1.hp
-        e2.hp-= e1.ad if e1.ad<e2.hp else e2.hp
+        e1.hp = max(0, e1.hp - e2.ad)
+        e2.hp = max(0, e2.hp - e1.ad)
         print(f'{e1.get_name()}: {e1.hp}hp')
         print(f'{e2.get_name()}: {e2.hp}hp')
         print('\n')
