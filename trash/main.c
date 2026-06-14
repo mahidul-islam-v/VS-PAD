9475787ee6fbc94c531e48f8ea91abd19bdd994c
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-06-14 15:19:22 +0000

    Jun 14, 2026, 9:19 PM

--- trash/main.c
+++ trash/main.c
@@ -1,33 +1,14 @@
-#include <stdio.h>
+#include <stdio.h> 
 int main() {
-     float salary, bonus = 0, final_salary;
-     int experience, rating;
 
-     printf("Enter salary: ");
-     scanf("%f", &salary);
-     printf("Enter years of experience: ");
-     scanf("%d", &experience);
-     printf("Enter performance rating: ");
-     scanf("%d", &rating);
+     int year;
+     printf("Year: ");
+     scanf("%d", &year);
 
-     if (experience >= 5) {
-         if (rating == 5) {
-             bonus = salary * 0.20;}
-         else if (rating == 4) {
-             bonus = salary * 0.15;}
-         else {
-             bonus = salary * 0.10;}}
+     if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+         printf("This is a leap year.\n");}
      else {
-         if (rating == 5) {
-             bonus = salary * 0.10; }
-         else if (rating == 4) {
-             bonus = salary * 0.07;}
-         else {
-             bonus = salary * 0.05;}}
+         printf("This is not a leap year.\n");}
 
-     final_salary = salary + bonus;
-     printf("Bonus: %.0f\n", bonus);
-     printf("Final Salary: %.0f\n", final_salary);
-
-     return 0;
+     return 0; 
 }
