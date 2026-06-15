97c5ab7ee1c09d6623aff8ff37a43d23eb99e71a
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-06-15 15:07:07 +0000

    Jun 15, 2026, 9:07 PM

--- FastAPI/FastAPI/books.py
+++ FastAPI/FastAPI/books.py
@@ -7,8 +7,12 @@ books = [
     {"title": "Title Two", "text": "World"}
 ]
 
+@app.get("/books")
+async def first_api():
+   return books
+
 @app.get("/books/{book_title}")
-async def first_api(book_title):
+async def second_api(book_title):
    for book in books:
       if book["title"].casefold() == book_title:
          return book
