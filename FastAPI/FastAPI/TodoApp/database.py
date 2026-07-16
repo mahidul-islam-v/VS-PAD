d14035cc593f1cfbe571099bc77b821ac5a17180
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-07-16 19:47:49 +0000

    Jul 17, 2026, 1:47 AM

--- FastAPI/FastAPI/TodoApp/database.py
+++ FastAPI/FastAPI/TodoApp/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 
@@ -7,3 +8,4 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+Base = declarative_base()
