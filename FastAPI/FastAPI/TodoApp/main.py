058cfe82c51ba91269acc0793087e557de9670dc
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-07-20 19:17:05 +0000

    Jul 21, 2026, 1:17 AM

--- FastAPI/FastAPI/TodoApp/main.py
+++ FastAPI/FastAPI/TodoApp/main.py
@@ -1,7 +1,14 @@
 import models 
 from fastapi import FastAPI
-from database import engine
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
