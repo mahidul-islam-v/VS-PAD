15671f25e6ac32ff4377274e0616722b4d2f97f9
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-07-20 20:34:35 +0000

    Jul 21, 2026, 2:34 AM

--- FastAPI/FastAPI/TodoApp/main.py
+++ FastAPI/FastAPI/TodoApp/main.py
@@ -2,7 +2,7 @@ from typing import Annotated
 from sqlalchemy.orm import Session
 import models
 from models import Todos
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from database import engine, SessionLocal
 
 app = FastAPI()
@@ -21,3 +21,12 @@ db_dependency = Annotated[Session, Depends(get_db)]
 @app.get("/")
 async def read_all(db: db_dependency):
    return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}")
+async def get_todo_by_id(db: db_dependency, todo_id: int):
+   todo_res = db.query(Todos).filter(Todos.id == todo_id).first()
+
+   if todo_res is not None:
+       return todo_res
+   
+   raise HTTPException(status_code = 404, detail='Todo id not found in db')
