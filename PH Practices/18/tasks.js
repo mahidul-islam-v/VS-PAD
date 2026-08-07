89e068aa85411a6f87d7de59762ecba4eb7770ff
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-08-07 19:12:53 +0000

    Aug 8, 2026, 1:12 AM

--- PH Practices/18/tasks.js
+++ PH Practices/18/tasks.js
@@ -2,6 +2,8 @@ const calculateBill = (amount, taxRate = 0.05) => amount+amount*taxRate
 
 const buildProfileCard = user => `${user.name}\n${user.title} at ${user.company}`
 
-const getLowestPrice = (prices) => Math.min([340, 120, 560, 90]);
+const getLowestPrice = prices => Math.min(prices)
 
 console.log(getLowestPrice([340, 120, 560, 90]));
+
+console.log(Math.min([1,2,3,4]))
