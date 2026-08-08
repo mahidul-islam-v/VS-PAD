ac937d9aaa53c8246b1ae087040755a1ca516bd3
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-08-08 14:01:38 +0000

    Aug 8, 2026, 8:01 PM

--- a.py
+++ a.py
@@ -5,6 +5,6 @@ gold, years, sons, daughters = map(int, input().split())
 for i in range(years):
 	gold = gold*0.975
 
-print(gold)
-
 x_factor = sons*2+daughters
+
+print(gold*2/x_factor, gold/x_factor)
