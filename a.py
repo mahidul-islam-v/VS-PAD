d30c9eeb35074c21dfe9873377afb09c5b8fa47a
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-08-08 15:23:44 +0000

    Aug 8, 2026, 9:23 PM

--- a.py
+++ a.py
@@ -5,4 +5,4 @@ n = int(input())
 for _ in range(n):
     angle, rad, side = map(float, input().split())
     angle_deg = angle*180/math.pi
-    print((math.sin(angle_deg)*side*side/2)-math.pi*rad*rad*angle_deg/360)
+    print(f"{((math.sin(angle)*side*side/2)-math.pi*rad*rad*angle_deg/360):.8f}")
