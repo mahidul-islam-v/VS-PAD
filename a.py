026016cc48386875ba26eddd1c20f283141ea668
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-08-08 14:52:56 +0000

    Aug 8, 2026, 8:52 PM

--- a.py
+++ a.py
@@ -4,12 +4,15 @@ for _ in range(n):
 	n_apple, queries = map(int, input().split())
 
 	belt = [int(x) for x in input().split()]
-	binary_belt = [0]*(n_apple+1)
+	binary_belt = [0]*(n_apple)
+	binary_belt[0] = 1 if belt[0]>=0 else 0
 
-	for i in range(n_apple):
-		binary_belt[i]
+	for i in range(1,n_apple):
+		binary_belt[i]= binary_belt[i-1]+(1 if belt[i]>=0 else 0)
+
+	print(binary_belt)
 
 	for _ in range(queries):
 		start, end = map(int, input().split())
 
-		print(sum(binary_belt[start-1:end]))
+		print(binary_belt[end-1]-binary_belt[start-1])
