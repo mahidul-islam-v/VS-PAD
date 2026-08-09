446ec6f03ab4a7917e44b777ad1b1364a75a211b
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-08-09 20:11:55 +0000

    Aug 10, 2026, 2:11 AM

--- PH Practices/18/taks2.js
+++ PH Practices/18/taks2.js
@@ -4,12 +4,14 @@ const getAvailableProducts = products => products.filter(product => product.stoc
 
 const findProductByName = (products, name) => products.find(product => product.name == name)
 
-console.log(
-    findProductByName(
-        (products = [
-            { name: "Pen", price: 10 },
-            { name: "Bag", price: 500 },
+const getTotalStockValue = products => products.reduce((accumulator, cur) => accumulator + cur.price * cur.stock, 0)
+
+const getDiscountedTotalForCategory = (products, category) => {
+    
+}
+    console.log(
+        getTotalStockValue([
+            { price: 50, stock: 4 },
+            { price: 20, stock: 10 },
         ]),
-        (name = "Bag"),
-    ),
-);
+    );
