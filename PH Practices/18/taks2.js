d986f9d61efaae56b294c46cf6e9ba6e0a3aa754
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-08-10 20:30:49 +0000

    Aug 11, 2026, 2:30 AM

--- PH Practices/18/taks2.js
+++ PH Practices/18/taks2.js
@@ -15,7 +15,7 @@ const applyBonusPoints = (users, bonus) => users.map(user =>{
 })
 
 const getTopSellingProduct = orders => {
-    obj = orders.reduce((accumulator, cur) => accumulator ? accumulator.unitsSold > cur.unitsSold : cur, undefined)
+    obj = orders.reduce((accumulator, cur) => accumulator.unitsSold > cur.unitsSold ? accumulator : cur, {unitsSold: 0})
     return obj.product
 }
 
