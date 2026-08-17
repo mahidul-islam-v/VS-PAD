1c1cfa7986b7288a014fe691f628d1f88d68764f
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-08-17 15:19:43 +0000

    Aug 17, 2026, 9:19 PM

--- PH Practices/21/temp.js
+++ PH Practices/21/temp.js
@@ -1,3 +1,3 @@
 "use strict";
-const printUser = (name, age) => console.log(`${name} is ${age} years old.`);
+const printUser = (name = "user", age = 0) => console.log(`${name} is ${age} years old.`);
 printUser("Amina", 22);
--- PH Practices/21/temp.ts
+++ PH Practices/21/temp.ts
@@ -1,3 +1,3 @@
-const printUser = (name: string, age: number) => console.log(`${name} is ${age} years old.`);
+const printUser = (name: string = "user", age: number = 0) => console.log(`${name} is ${age} years old.`);
 
 printUser("Amina", 22);
