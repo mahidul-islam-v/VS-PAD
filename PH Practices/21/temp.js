c09dfea55f8728398f0675e34aceeeee9a664c09
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-08-21 18:21:44 +0000

    Aug 22, 2026, 12:21 AM

--- PH Practices/21/temp.js
+++ PH Practices/21/temp.js
@@ -1,8 +1,1 @@
 "use strict";
-const printUser = (name = "user", age = 0) => console.log(`${name} is ${age} years old.`);
-const nayme = ["Me", 2020];
-const mailTo = (obj) => obj.name + ' ' + (obj.mail ? obj.mail : "Not given");
-const chckr = (numba) => numba % 2 == 0 ? "even" : "odd";
-const summr = (...numba) => numba.reduce((acc, curr) => acc + curr, 0);
-const mrgr = (...numbers) => [...numbers];
-console.log(mrgr(1, 2, 3, 411, 22, 33, 44));
