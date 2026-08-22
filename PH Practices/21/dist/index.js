a85b4a88c49ab48767fce269ae7d658393359688
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-08-22 02:18:07 +0000

    Aug 22, 2026, 8:18 AM

--- PH Practices/21/dist/index.js
+++ PH Practices/21/dist/index.js
@@ -3,5 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let val = "Hello TypeScript";
 let numba = "100";
 const number = typeof (numba) === "number" ? numba : Number(numba);
-console.log(number);
+const p1 = {
+    title: "one",
+    price: 9.99,
+    inStock: true,
+};
+const p2 = {
+    title: "Two",
+    price: 0.99
+};
+function printPrice(product) {
+    console.log(product.price);
+}
+printPrice(p1);
+printPrice(p2);
 //# sourceMappingURL=index.js.map
--- PH Practices/21/src/index.ts
+++ PH Practices/21/src/index.ts
@@ -2,6 +2,27 @@ let val: unknown = "Hello TypeScript";
 
 let numba: string | number = "100"
 
-const number = typeof(numba) === "number" ? numba : Number(numba)
+const number = typeof (numba) === "number" ? numba : Number(numba)
 
-console.log(number+10);
+interface Product {
+    title: string;
+    price: number;
+    inStock?: boolean;
+}
+
+const p1: Product = {
+    title: "one",
+    price: 9.99,
+    inStock: true,
+}
+const p2: Product = {
+    title: "Two",
+    price: 0.99
+};
+
+function printPrice(product:Product):void {
+    console.log(product.price)
+}
+
+printPrice(p1);
+printPrice(p2);
