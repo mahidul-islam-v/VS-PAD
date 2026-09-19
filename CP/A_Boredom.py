983633f22221563bbb965c7198b638b54449e2c1
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-09-19 18:49:43 +0000

    Sep 20, 2026, 12:49 AM

--- CP/A_Boredom.py
+++ CP/A_Boredom.py
@@ -1,15 +1,5 @@
 n = int(input())
 
-for i in range(1, n+1):
-    fact = 1
-    for j in range(1, i+1):
-        fact*=j
-    print(fact)
-
-# s = str(fact)
-# z_count = 0
-# for i in range(len(s)):
-#     if s[-(i+1)] == "0":
-#         z_count+= 1
-#     else:
-#         break
+for _ in range(n):
+    [a, b] = [int(x) for x in input().split()]
+    print("YES" if (a+b)%3==0 and ((2*a)>=b and ((2*b)>=a)) else "NO")
