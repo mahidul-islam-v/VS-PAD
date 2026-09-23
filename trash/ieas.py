2a2636627bd1fa356405b5aa978ff1592c9aa1ea
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-09-23 23:16:33 +0000

    Sep 24, 2026, 5:16 AM

--- trash/ieas.py
+++ trash/ieas.py
@@ -3,5 +3,39 @@ from collections import Counter
 s = list(input())
 
 counts = Counter(s)
+keys = list(counts.elements())
+values = list(counts.values())
+lent = len(keys)
+ans = ""
 
-print(counts.values())
+pallable = True
+if (lent%2==1):
+    cc = 0
+    for i in values :
+        if i%2!=0 and cc>0:
+            cc+= 1
+            pallable = False
+            break;
+else :
+    for i in list(counts.values()) :
+        if i%2!=0:
+            pallable = False
+            break;
+
+if not pallable:
+    print("NO SOLUTION")
+else :
+    if (lent%2==1):
+        for i in range(lent) :
+            for k in range(int(values[i]/2)+1):
+                ans+= keys[i]
+
+        for i in range(lent) :
+                    for k in range(int(values[i-i-1]/2)+1):
+                        ans+= keys[lent-i-1]
+
+
+    
+
+
+print(keys, values)
