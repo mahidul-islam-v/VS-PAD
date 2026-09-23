a6284edb1b30420b2cdb01e488f36da94eacfb02
mahidul-islam-v <mahidulislam.00713@gmail.com>
2026-09-23 23:31:50 +0000

    Sep 24, 2026, 5:31 AM

--- trash/ieas.py
+++ trash/ieas.py
@@ -6,6 +6,7 @@ counts = Counter(s)
 keys = list(counts.keys())
 values = list(counts.values())
 lent = len(keys)
+shum = sum(values)
 ans = ""
 
 pallable = True
@@ -25,8 +26,7 @@ else :
 if not pallable:
     print("NO SOLUTION")
 else :
-    if (lent%2==1):
-        print(values)
+    if (shum%2==1):
         for i in range(lent-1) :
             for k in range(int(values[i]//2)):
                 ans+= keys[i]
@@ -35,11 +35,10 @@ else :
             for k in range(int(values[-j-2]/2)):
                 ans+= keys[-j-2]
 
-    if (lent%2==0):
+    if (shum%2==0):
             for i in range(lent) :
                 for k in range(int(values[i]//2)):
                     ans+= keys[i]
-                    print(values[i])
             for j in range(lent) :
                 for k in range(int(values[-j-1]/2)):
                     ans+= keys[-j-1]
